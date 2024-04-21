088d92fd5d20e516d02985e4b0d2ba99eaeaf5fe
GuiAgapito <guilherme.agapito2004@gmail.com>
2024-04-21 17:11:42 -0300

    Update

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 // Get the necessary elements from the DOM
-const viewPassword = document.getElementById("viewPassword");
-const lengthInput = document.getElementById("lengthPassword");
-const infoLength = document.getElementById("infoLength");
-const uppercaseTextInput = document.getElementById("uppercaseText");
-const lowercaseTextInput = document.getElementById("lowercaseText");
+const viewPassword = document.getElementById("view-password");
+const lengthInput = document.getElementById("length-password");
+const uppercaseTextInput = document.getElementById("uppercase-text");
+const lowercaseTextInput = document.getElementById("lowercase-text");
 const numbersInput = document.getElementById("numbers");
 const symbolsInput = document.getElementById("symbols");
-const btnGenerate = document.getElementById("btnGenerate");
-const iconCopy = document.getElementById("iconCopy");
-const alert = document.getElementById("alert");
+const btnGenerate = document.getElementById("btn-generate");
+const iconCopy = document.getElementById("btn-copy");
+const message = document.getElementById("message");
 
 // Define arrays for numbers, symbols, lowercase and uppercase letters
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const symbols = ["!", "@", "#", "$", "%", "&", "_"];
-const text = Array.from(Array(26)).map((_, i) => i + 97);
-const lowercaseText = text.map((item) => String.fromCharCode(item));
-const uppercaseText = lowercaseText.map((item) => item.toUpperCase());
+const lowercaseText = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseText = lowercaseText.toUpperCase();
 
-// Set the initial value of infoLength
-infoLength.innerHTML = lengthInput.value;
-
-// Update infoLength when lengthInput value changes
-lengthInput.addEventListener("change", () => {
-  infoLength.innerHTML = lengthInput.value;
-});
+const filter_numbers = () => {
+  lengthInput.value = lengthInput.value.split('').filter(item => parseInt(item) in numbers).join('');
+  return lengthInput;
+} 
 
 // Generate password when btnGenerate is clicked
 btnGenerate.addEventListener("click", () => {
-  if (lengthInput.value == "") {
-    alert.style.display = "flex";
-    alert.textContent = "Indique a quantidade de caracteres!";
+  if ((lengthInput.value == 0 || lengthInput.value == "" || lengthInput.value > 1000) && (uppercaseTextInput.checked == false && lowercaseTextInput.checked == false && numbersInput.checked == false && symbolsInput.checked == false)) {
+    message.style.display = "flex";
+    message.textContent = "Indique a quantidade de caracteres entre 1 e 1000 e selecione pelo menos um tipo de caractere! ";
+    message.className = "message active";
+    setTimeout(() => {      
+      message.className = "message";
+    }, 5000);
+    viewPassword.value = '';
+  } else if (lengthInput.value == 0 || lengthInput.value == "" || lengthInput.value > 1000) {
+    message.style.display = "flex";
+    message.textContent = "Indique a quantidade de caracteres entre 1 e 1000!";
+    message.className = "message active";
     setTimeout(() => {
-      alert.style.display = "none";
+      message.className = "message";
     }, 2000);
+    viewPassword.value = '';
+  } else if (uppercaseTextInput.checked == false && lowercaseTextInput.checked == false && numbersInput.checked == false && symbolsInput.checked == false) {
+    message.style.display = "flex";
+    message.textContent = "Selecione pelo menos um tipo de caractere!";
+    message.className = "message active";
+    setTimeout(() => {
+      message.className = "message";
+    }, 2000);    
+    viewPassword.value = '';
+  } else {
+    generatePassword(
+      uppercaseTextInput.checked,
+      lowercaseTextInput.checked,
+      numbersInput.checked,
+      symbolsInput.checked,
+      lengthInput.value
+    );
   }
-  
-  generatePassword(
-    uppercaseTextInput.checked,
-    lowercaseTextInput.checked,
-    numbersInput.checked,
-    symbolsInput.checked,
-    lengthInput.value
-  );
 });
 
 // Function to generate a password based on the selected criteria
@@ -53,12 +66,11 @@ const generatePassword = (
   length
 ) => {
   // Create an array containing the selected character sets
-  const newArray = [
-    ...(hasNumbers ? numbers : []),
-    ...(hasSymbols ? symbols : []),
-    ...(hasLowercase ? lowercaseText : []),
-    ...(hasUpperCase ? uppercaseText : []),
-  ];
+  let newArray = '';
+  if (hasUpperCase) newArray += uppercaseText;
+  if (hasLowercase) newArray += lowercaseText;
+  if (hasNumbers) newArray += numbers.join('');
+  if (hasSymbols) newArray += symbols.join('');
 
   // If no character sets are selected, return
   if (newArray.length === 0) return;
@@ -84,17 +96,19 @@ iconCopy.addEventListener("click", () => {
     document.execCommand("copy");
 
     // Display the "Senha copiada!" alert for 2 seconds
-    alert.style.display = "flex";
-    alert.textContent = "Senha copiada!";
+    message.style.display = "initial";
+    message.textContent = "Senha copiada!";
+    message.className = "message active";
     setTimeout(() => {
-      alert.style.display = "none";
-    }, 2000);
+      message.className = "message";
+    }, 3000);
   } else {
     // Display the error alert for 2 seconds
-    alert.style.display = "flex";
-    alert.textContent = "Erro ao copiar, pois não foi gerada nenhuma senha!";
+    message.style.display = "flex";
+    message.textContent = "Erro ao copiar, pois não foi gerada nenhuma senha!";
+    message.className = "message active";
     setTimeout(() => {
-      alert.style.display = "none";
-    }, 2000);
+      message.className = "message";
+    }, 3000);
   }
 });
